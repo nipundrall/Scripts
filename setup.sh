6e592bafaaa596ef67efa6379af75f558c855280
NIPUN DRALL <161411550+nipundrall@users.noreply.github.com>
2024-05-11 14:48:55 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -7,15 +7,15 @@ sudo pacman -S cowsay
 
 #Installing Dependencies (arch only)
 #echo "Installing Dependencies"
-cowsay Im installing Dependencies
+cowsay "I'm installing Dependencies"
 sudo pacman -S hyprland nautilus eog gimp libreoffice-still discord kitty neofetch pulsemixer pavucontrol waybar git vim hyprpaper hyprlock hypridle wofi gparted cliphist gnome-disk-utility gnome-weather gnome-calculator swaync grub-customizer polkit-kde-agent ttf-jetbrains-mono-nerd ttf-jetbrains-mono qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 &&sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -S google-chrome wlogout nerd-fonts-complete ttf-icomoon-feather wayland-logout 
 
 #Download Dots
 #echo "Downloading Dotfiles"
-cowsay Now Im downloading Dotfiles
+cowsay "Now I'm downloading Dotfiles"
 cd && git clone https://github.com/nipundrall/NDS-Hyprland.git
 
-Cowsay Don't Look like this I'm Working
+Cowsay "Don't Look like this I'm Working"
 mv NDS-Hyprland ndshypr
 
 cd $ndshypr
@@ -38,11 +38,11 @@ cowsay Now Adding Permisions
 #cd $HOME/.config
 #git clone https://gitlab.com/thelinuxcast/nvim.git
 
-cowsay Ive Done Everything You Said Now Updating.
+
 
 #Updating 
 echo "Final Updates"
-sudo pacman -Syu && yay -Syu 
+sudo pacman -Syu && yay -Syu
 
-echo " Everthing Done"
+cowsay "I've Done Everything You Said"
 
