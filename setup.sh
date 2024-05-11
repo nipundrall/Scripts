f74f70adb566e2abad79d5315d82a3bdcfdee8dd
NIPUN DRALL <161411550+nipundrall@users.noreply.github.com>
2024-05-11 14:55:32 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ ndshypr="$HOME/ndshypr"
 #gitthings="$HOME/Downloads/gitthings"
 #scripts="$HOME/Downloads/gitthings/scripts"
 sudo pacman -S cowsay
+# cowsay Installed
+cowsay "Sit Back and Relax I'll Do everthing"
 
 #Installing Dependencies (arch only)
 #echo "Installing Dependencies"
-cowsay "I'm installing Dependencies"
+cowsay "I'll Start with installing Dependencies"
 sudo pacman -S hyprland nautilus eog gimp libreoffice-still discord kitty neofetch pulsemixer pavucontrol waybar git vim hyprpaper hyprlock hypridle wofi gparted cliphist gnome-disk-utility gnome-weather gnome-calculator swaync grub-customizer polkit-kde-agent ttf-jetbrains-mono-nerd ttf-jetbrains-mono qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 &&sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -S google-chrome wlogout nerd-fonts-complete ttf-icomoon-feather wayland-logout 
 
 #Download Dots
@@ -29,7 +31,11 @@ sudo cp -r $HOME/ndshypr/sddm/sddm.conf.d /etc/
 sudo cp -r $HOME/ndshypr/sddm/sddm/themes/Sugar-Candy /usr/share/sddm/themes
 sudo cp -r $HOME/ndshypr/grub/NDSGrub /boot/grub/themes
 
-cowsay Now Adding Permisions
+cowsay "Now Adding Permisions"
+
+sudo chmod +x ~/.config/waybar/modules/mediaplayer.py
+sudo chmod +x ~/.config/waybar/modules/nt_tray.sh
+sudo chmod +x ~/.config/waybar/modules/launch.sh
 
 #cd $gitthings && git clone https://gitlab.com/thelinuxcast/scripts.git
 #cd $scripts
@@ -45,4 +51,6 @@ echo "Final Updates"
 sudo pacman -Syu && yay -Syu
 
 cowsay "I've Done Everything You Said"
+
+cowsay " Long Live $USER "
 
